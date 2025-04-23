c8ae7204baef9f8aa3448863859107ce9c53f40e
DPW2005 <prince.dongmo@enspy-uy1.cm>
2025-04-24 01:27:00 +0200

    Separation des interfaces informations et des tableaux

--- src/main/java/org/example/medical/MedecinContainer.java
+++ src/main/java/org/example/medical/MedecinContainer.java
@@ -9,6 +9,7 @@ import jade.wrapper.AgentContainer ;
 import jade.wrapper.AgentController;
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
@@ -17,6 +18,9 @@ import javafx.stage.Stage;
 import org.example.medical.model.Patient;
 
 public class MedecinContainer extends Application {
+
+    AgentContainer medecinContainer ;
+
     public static void main(String[] args){
         launch(MedecinContainer.class) ;
     }
@@ -24,12 +28,18 @@ public class MedecinContainer extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         startContainer();
-        VBox layout = new VBox(10) ;
-        layout.setSpacing(15);
-        layout.setPadding(new Insets(20));
-        layout.setStyle("-fx-background-color : #f5f5f5 ;");
+        VBox info = new VBox(10) ;
+        info.setSpacing(15);
+        info.setPadding(new Insets(20));
+        info.setStyle("-fx-background-color : #f5f5f5 ;");
+        VBox tableau = new VBox(10) ;
+        tableau.setSpacing(15);
+        tableau.setPadding(new Insets(20));
+        tableau.setStyle("-fx-background-color : #f5f5f5 ;");
         Label title = new Label("Medecin App") ;
         title.setStyle("-fx-font-size : 18 ; -fx-font-weight : bold ;");
+        Label tableTitle = new Label("Consultation a faire") ;
+        tableTitle.setStyle("-fx-font-size : 16 ; -fx-font-weight : bold ;");
 
         GridPane form = new GridPane() ;
         form.setVgap(10) ;
@@ -43,7 +53,19 @@ public class MedecinContainer extends Application {
         form.addRow(0,new Label("Nom : "),nameField) ;
         form.addRow(1,new Label("Service : "),serviceField) ;
 
+        Button connexionButton = new Button("Envoyer information") ;
+        connexionButton.setAlignment(Pos.CENTER) ;
         Button askingButton = new Button("Consulter") ;
+
+        askingButton.setOnAction(event -> {
+            try{
+                AgentController agentController = medecinContainer.createNewAgent("medecin", MedecinAgent.class.getName(), new Object[]{}) ;
+                agentController.start() ;
+            }
+            catch(Exception e){
+                e.printStackTrace() ;
+            }
+        });
 
         TableColumn<Patient,String> nameCol = new TableColumn<>("Nom") ;
         TableColumn<Patient,String> firstNameCol = new TableColumn<>("Prenom") ;
@@ -58,10 +80,17 @@ public class MedecinContainer extends Application {
         patientTable.getColumns().addAll(nameCol,firstNameCol,ageCol,sexeCol,weightCol,heightCol,temperatureCol,tensionCol) ;
         patientTable.setPlaceholder(new Label("Aucun contenu dans la table"));
 
-        layout.getChildren().addAll(title,form,disponibleCheck,new Label("Information sur la consultation"),patientTable,askingButton) ;
-        Scene scene = new Scene(layout,600,600) ;
-        stage.setScene(scene) ;
+        info.getChildren().addAll(title,form,connexionButton) ;
+        tableau.getChildren().addAll(title,tableTitle,patientTable,askingButton) ;
+        Scene scene1 = new Scene(info,300,300) ;
+        Scene scene2 = new Scene(tableau,400,400) ;
+        stage.setScene(scene1) ;
         stage.show() ;
+        connexionButton.setOnAction(event ->{
+            stage.close();
+            stage.setScene(scene2);
+            stage.show();
+        });
     }
 
     public void startContainer() throws Exception {
@@ -69,8 +98,6 @@ public class MedecinContainer extends Application {
         Properties properties = new ExtendedProperties() ;
         properties.setProperty(Profile.GUI, "false") ;
         Profile profile = new ProfileImpl(properties) ;
-        AgentContainer medecinContainer = runtime.createAgentContainer(profile) ;
-        AgentController agentController = medecinContainer.createNewAgent("medecin", MedecinAgent.class.getName(), new Object[]{}) ;
-        agentController.start() ;
+        medecinContainer = runtime.createAgentContainer(profile) ;
     }
 }
--- src/main/java/org/example/medical/SecretaireContainer.java
+++ src/main/java/org/example/medical/SecretaireContainer.java
@@ -9,6 +9,7 @@ import jade.wrapper.AgentContainer ;
 import jade.wrapper.AgentController;
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
@@ -19,6 +20,9 @@ import org.example.medical.model.Medecin;
 import org.example.medical.model.Patient;
 
 public class SecretaireContainer extends Application {
+
+    AgentContainer secretaireContainer ;
+
     public static void main(String[] args){
         launch(SecretaireContainer.class) ;
     }
@@ -26,15 +30,23 @@ public class SecretaireContainer extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         startContainer();
-        VBox layout = new VBox(10) ;
         HBox table = new HBox(10) ;
-        layout.setSpacing(15) ;
         table.setSpacing(15) ;
-        layout.setPadding(new Insets(20)) ;
         table.setPadding(new Insets(20)) ;
-        layout.setStyle("-fx-background-color : #f5f5f5 ;");
+        VBox info = new VBox(10) ;
+        info.setSpacing(15);
+        info.setPadding(new Insets(20));
+        info.setStyle("-fx-background-color : #f5f5f5 ;");
+        VBox tableau = new VBox(10) ;
+        tableau.setSpacing(15);
+        tableau.setPadding(new Insets(20));
+        tableau.setStyle("-fx-background-color : #f5f5f5 ;");
         Label title = new Label("Secretary App") ;
         title.setStyle("-fx-font-size : 18 ; -fx-font-weight : bold ;");
+        Label patientTitle = new Label("Consultation a faire") ;
+        patientTitle.setStyle("-fx-font-size : 16 ; -fx-font-weight : bold ;");
+        Label medecinTitle = new Label("Consultation a faire") ;
+        medecinTitle.setStyle("-fx-font-size : 16 ; -fx-font-weight : bold ;");
 
         GridPane form = new GridPane() ;
         form.setVgap(10) ;
@@ -46,7 +58,19 @@ public class SecretaireContainer extends Application {
         form.addRow(0,new Label("ID : "),idField) ;
         form.addRow(1,new Label("Nom : "),nameField) ;
 
+        Button connexionButton = new Button("Envoyer information") ;
+        connexionButton.setAlignment(Pos.CENTER) ;
         Button askingButton = new Button("Affecter consultation") ;
+
+        askingButton.setOnAction(event -> {
+            try{
+                AgentController agentController = secretaireContainer.createNewAgent("secretaire", SecretaireAgent.class.getName(), new Object[]{}) ;
+                agentController.start() ;
+            }
+            catch(Exception e){
+                e.printStackTrace() ;
+            }
+        });
 
         TableColumn<Patient,String> nameCol = new TableColumn<>("Nom") ;
         TableColumn<Patient,String> firstNameCol = new TableColumn<>("Prenom") ;
@@ -62,10 +86,17 @@ public class SecretaireContainer extends Application {
         medecinTable.setPlaceholder(new Label("Aucun contenu dans la table"));
 
         table.getChildren().addAll(new VBox(new Label("Patients"),patientTable),new VBox(new Label("Medecins"),medecinTable)) ;
-        layout.getChildren().addAll(title,form,table,askingButton) ;
-        Scene scene = new Scene(layout,600,600) ;
-        stage.setScene(scene) ;
+        info.getChildren().addAll(title,form,connexionButton) ;
+        tableau.getChildren().addAll(title,table,askingButton) ;
+        Scene scene1 = new Scene(info,300,300) ;
+        Scene scene2 = new Scene(tableau,600,600) ;
+        stage.setScene(scene1) ;
         stage.show() ;
+        connexionButton.setOnAction(event ->{
+            stage.close();
+            stage.setScene(scene2);
+            stage.show();
+        });
     }
 
     public void startContainer() throws Exception {
@@ -73,8 +104,6 @@ public class SecretaireContainer extends Application {
         Properties properties = new ExtendedProperties() ;
         properties.setProperty(Profile.GUI, "false") ;
         Profile profile = new ProfileImpl(properties) ;
-        AgentContainer secretaireContainer = runtime.createAgentContainer(profile) ;
-        AgentController agentController = secretaireContainer.createNewAgent("secretaire", SecretaireAgent.class.getName(), new Object[]{}) ;
-        agentController.start() ;
+        secretaireContainer = runtime.createAgentContainer(profile) ;
     }
 }
